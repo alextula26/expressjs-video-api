ec07c5d1db3d891149759692fc3ede5000d56918
alextula26 <alex_marc86@mail.ru>
2022-11-13 00:08:16 +0300

    Добавил метод put

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import moment from 'moment'
 import { isEmpty } from 'lodash'
 import { getNextId, getErrors } from './utils'
+import { HTTPStatuses } from './types'
 import { db } from './mocks'
 
 const app = express()
@@ -12,16 +13,16 @@ app.use(jsonBodyMiddleware)
 
 app
   .get('/', (_, res) => {
-    res.status(200).send(db.videos)
+    res.status(HTTPStatuses.SUCCESS200).send(db.videos)
   })
   .get('/api/videos', (_, res) => {
-    res.status(200).send(db.videos)
+    res.status(HTTPStatuses.SUCCESS200).send(db.videos)
   })
   .get('/api/videos/:id', (req, res) => {
     const video = db.videos.find(({ id }) => id === Number(req.params.id))
     
     if (!video) {
-      res.status(404)
+      res.status(HTTPStatuses.NOTFOUND404)
       return
     }
 
@@ -30,10 +31,8 @@ app
   .post('/api/videos', (req, res) => {
     const errors = getErrors(req.body)
     
-    console.log
-
     if (!isEmpty(errors)) {
-      res.status(400).send(errors)
+      res.status(HTTPStatuses.BADREQUEST400).send(errors)
       return
     }    
     
@@ -49,24 +48,49 @@ app
     }
 
     db.videos.push(item)
-    res.status(201).send(item)
+    res.status(HTTPStatuses.CREATED201).send(item)
   })
-  .put('/api/videos', (req, res) => {
-    res.send('put videos')
+  .put('/api/videos/:id', (req, res) => {
+    const errors = getErrors(req.body)
+
+    if (!isEmpty(errors)) {
+      res.status(HTTPStatuses.BADREQUEST400).send(errors)
+      return
+    }
+    
+    const video = db.videos.find(({ id }) => id === Number(req.params.id))
+
+    if (!video) {
+      res.status(HTTPStatuses.NOTFOUND404).send()
+      return
+    }
+
+    const id = Number(req.params.id)
+    const item = {
+      id,
+      title: req.body.title,
+      author: req.body.author,
+      availableResolutions: req.body.availableResolutions,
+      canBeDownloaded: req.body.canBeDownloaded,
+      minAgeRestriction: req.body.minAgeRestriction,
+      createdAt: video.createdAt,
+      publicationDate: req.body.publicationDate,
+    }
+
+    db.videos.map(video => video.id === id ? item : video)
+    res.status(HTTPStatuses.NOCONTENT204).send()
   })
   .delete('/api/videos/:id', (req, res) => {
     const video = db.videos.find(({ id }) => id === Number(req.params.id))
     
     if (!video) {
-      res.status(404)
+      res.status(HTTPStatuses.NOTFOUND404)
       return
     }
     
     db.videos.filter(({ id }) => id !== video.id)
-    res.status(204)
+    res.status(HTTPStatuses.NOCONTENT204)
   })
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  .listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
