9aa6f6a7e43b77acfe7b3381e153c2d7360b1dc0
alextula26 <alex_marc86@mail.ru>
2022-12-31 16:57:26 +0300

    Добавил отправку письма по email и проверка по коду

--- src/managers/email-manager.ts
+++ src/managers/email-manager.ts
@@ -1,13 +1,16 @@
 import { emailAdapter } from "../adapters"
-import { UserType } from '../types'
 
 export const emailManager = {
-  async sendEmailCreatedUser(user: UserType) {
-    const messageId: string | null = await emailAdapter.sendEmail(
-      user.email,
-      'Подтверждение регистрации',
-      '<h1>Hello guys</h1><div><a href=\"https://it-incubator.io/\">Click</a></div>'
-    )
+  async sendEmailCreatedUser(email: string, code: string) {
+    const message = `
+      <h1>Thank for your registration</h1>
+      <p>To finish registration please follow the link below:
+        <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>
+      </p>
+      `
+    const subject = 'Подтверждение регистрации' 
+
+    const messageId: string | null = await emailAdapter.sendEmail(email, subject, message)
 
     if (!messageId) {
       return false
